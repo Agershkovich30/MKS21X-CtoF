87011d6146e2bfc6fb3dd00761c58e0525197bb2
Aaron Gershkovich <agershkovich30@stuy.edu>
2021-09-23 22:38:18 -0400

    [Test]

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,20 @@
+public class CtoFTester{
+  /*Questions:
+  1) The parameters should be the temperature of the undesired for,
+  so that it can be converted. So for celsiusToFahrenheit,
+  the temperature should be entered in celsius form, but in
+  fahrenheitToCelsius, it should have the Fahrenheit temperature
+  as the parameter.
+  2) The function should have doubles as parameters
+  because they make the most sense as inputs, as integers,
+  the only other comprable idea only allows certain inputs,
+  and not as much specificity.
+  3) The functions should return a double type of the temperature given
+  in the other form of it, so Celsius to Fahrenheit and vice versa.
+  */
 
+
+public static void main (String[] args){
+
+}
+}
